77e0e4e4b5cda9ba9335fe0fafb81a34ec128832
Alex Terskin <terskinalex@gmail.com>
2024-09-28 16:05:44 -0400

    revert changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,26 +4,45 @@ import NavBar from "./component/navbar";
 const Home = () => {
   return (
     <div>
-      {/* <NavBar /> */}
+      <NavBar />
       <div className="overview-text">
-        {/* <h1 style={{ fontWeight: "bold", fontSize: "2em" }}>RAG QnA Chatbot</h1> */}
-        {/* <br /> */}
+        <h1 style={{ fontWeight: "bold", fontSize: "2em" }}>RAG QnA Chatbot</h1>
+        <br />
 
-        <div className="flex justify-center items-center h-screen bg-gray-100">
-          <div className="text-center bg-white p-8 rounded-lg shadow-md max-w-md w-full">
-            <h1 className="text-2xl font-bold mb-5">
-              Are you allergic to any medication?
-            </h1>
-            <input
-              type="text"
-              placeholder="Your Answer"
-              className="w-full px-3 py-2 mb-5 border border-gray-300 rounded-md text-base focus:outline-none focus:ring-2 focus:ring-black"
-            />
-            <button className="px-5 py-2 bg-black text-white rounded-md text-base cursor-pointer transition duration-300 ease-in-out hover:bg-gray-800">
-              NEXT
-            </button>
-          </div>
-        </div>
+        <p>
+          With this RAG (Retrieval-Augmented Generation) application, you can
+          quickly deploy a chatbot that is enhanced by your own data. By simply
+          uploading any PDF file(s) of your choosing, you will be creating your
+          own custom Questions and Answers (QnA) chatbot.
+        </p>
+        <br />
+
+        <h2 style={{ fontStyle: "italic" }}>Getting Started</h2>
+        <ul>
+          <li>Navigate to the &ldquo;Train&rdquo; tab above.</li>
+          <li>
+            Here you can upload any number of PDF files you wish to ask the
+            chatbot about, or that you wish the chatbot would consider when
+            providing an answer.
+          </li>
+          <li>
+            After uploading your files, navigate to &ldquo;QnA&rdquo; tab.
+          </li>
+          <li>
+            In the prompt, you can now ask questions about the uploaded PDF
+            files in natural language, and receive a response.
+          </li>
+        </ul>
+
+        <br />
+
+        <p>
+          This application leverages MongoDB&apos;s robust vector store
+          capabilities. Each uploaded PDF is embedded in a vectorized format
+          into your MongoDB Atlas cluster. The vector store efficiently
+          organizes and indexes data, streamlining the process of generating
+          responses and insights from the RAG model.
+        </p>
       </div>
     </div>
   );
