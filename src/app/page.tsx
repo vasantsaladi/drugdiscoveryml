c2048a87505075b06af507721f10742cc5401cb5
Alex Terskin <terskinalex@gmail.com>
2024-09-28 16:01:08 -0400

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,34 +4,26 @@ import NavBar from "./component/navbar";
 const Home = () => {
   return (
     <div>
-      <NavBar />
+      {/* <NavBar /> */}
       <div className="overview-text">
-        <h1 style={{ fontWeight: "bold", fontSize: "2em" }}>RAG QnA Chatbot</h1>
-        <br />
+        {/* <h1 style={{ fontWeight: "bold", fontSize: "2em" }}>RAG QnA Chatbot</h1> */}
+        {/* <br /> */}
 
-        <p>
-          <input type="text" placeholder="Input Disease" />
-        </p>
-        <br />
-
-        {/* <h2 style={{ fontStyle: "italic" }}>Getting Started</h2> */}
-        {/* <ul>
-          <li>Navigate to the &ldquo;Train&rdquo; tab above.</li>
-          <li>
-            Here you can upload any number of PDF files you wish to ask the
-            chatbot about, or that you wish the chatbot would consider when
-            providing an answer.
-          </li>
-          <li>
-            After uploading your files, navigate to &ldquo;QnA&rdquo; tab.
-          </li>
-          <li>
-            In the prompt, you can now ask questions about the uploaded PDF
-            files in natural language, and receive a response.
-          </li>
-        </ul> */}
-
-        <br />
+        <div className="flex justify-center items-center h-screen bg-gray-100">
+          <div className="text-center bg-white p-8 rounded-lg shadow-md max-w-md w-full">
+            <h1 className="text-2xl font-bold mb-5">
+              Are you allergic to any medication?
+            </h1>
+            <input
+              type="text"
+              placeholder="Your Answer"
+              className="w-full px-3 py-2 mb-5 border border-gray-300 rounded-md text-base focus:outline-none focus:ring-2 focus:ring-black"
+            />
+            <button className="px-5 py-2 bg-black text-white rounded-md text-base cursor-pointer transition duration-300 ease-in-out hover:bg-gray-800">
+              NEXT
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
