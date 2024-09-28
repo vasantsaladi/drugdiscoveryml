5503dae258270a769242286b9fe561dfa342291b
Alex Terskin <terskinalex@gmail.com>
2024-09-28 15:36:35 -0400

    add form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,12 @@ const Home = () => {
         <br />
 
         <p>
-          With this RAG (Retrieval-Augmented Generation) application, you can
-          quickly deploy a chatbot that is enhanced by your own data. By simply
-          uploading any PDF file(s) of your choosing, you will be creating your
-          own custom Questions and Answers (QnA) chatbot.
+          <input type="text" placeholder="Input Disease" />
         </p>
         <br />
 
-        <h2 style={{ fontStyle: "italic" }}>Getting Started</h2>
-        <ul>
+        {/* <h2 style={{ fontStyle: "italic" }}>Getting Started</h2> */}
+        {/* <ul>
           <li>Navigate to the &ldquo;Train&rdquo; tab above.</li>
           <li>
             Here you can upload any number of PDF files you wish to ask the
@@ -32,17 +29,9 @@ const Home = () => {
             In the prompt, you can now ask questions about the uploaded PDF
             files in natural language, and receive a response.
           </li>
-        </ul>
+        </ul> */}
 
         <br />
-
-        <p>
-          This application leverages MongoDB&apos;s robust vector store
-          capabilities. Each uploaded PDF is embedded in a vectorized format
-          into your MongoDB Atlas cluster. The vector store efficiently
-          organizes and indexes data, streamlining the process of generating
-          responses and insights from the RAG model.
-        </p>
       </div>
     </div>
   );
